fb509496074b4963ef6dda85bdf194ab26b199f9
Cristhian de Leon <cdeleonv3@miumg.edu.gt>
2019-11-01 16:57:17 -0600

    Procedimiento para imprimir directamente

--- proyecto/ModuloReporte/CapaDiseno/Procesos/Frm_VistaReporte.cs
+++ proyecto/ModuloReporte/CapaDiseno/Procesos/Frm_VistaReporte.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Windows.Forms;
 using CrystalDecisions.CrystalReports.Engine;
-using CrystalDecisions.Shared;
+using System.Drawing.Printing;
 
 //Cristhian de Leon 0901 15 6167
 
@@ -19,6 +19,7 @@ namespace CapaDisenoRpt.Procesos
         public Frm_VistaReporte(string filePath)
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
             generarRpt(filePath);
         }
 
@@ -33,30 +34,11 @@ namespace CapaDisenoRpt.Procesos
             Crv_Reporte.Refresh();
         }
 
-        private void guardarEnSistema()
+        private void imprimirRpt(string filePath)
         {
-            {
-                rptDoc = new ReportDocument();
-                DiskFileDestinationOptions dest = new DiskFileDestinationOptions();
-                rptDoc.FileName = "pruebas";
-                dest.DiskFileName = @"C:\tmp\pruebas.pdf";
-
-                PdfFormatOptions formatOpt = new PdfFormatOptions();
-                formatOpt.FirstPageNumber = 0;
-                formatOpt.LastPageNumber = 10;
-                formatOpt.UsePageRange = false;
-                formatOpt.CreateBookmarksFromGroupTree = false;
-                formatOpt.PrintOnLoad = false;
-
-                ExportOptions ex = new ExportOptions();
-                ex.ExportDestinationType = ExportDestinationType.DiskFile;
-                ex.ExportDestinationOptions = dest;
-                ex.ExportFormatType = ExportFormatType.PortableDocFormat;
-                ex.ExportFormatOptions = formatOpt;
-                //rptDoc.Export(ex);
-                //rptDoc.ExportToDisk(ExportFormatType.PortableDocFormat, @"D:\tmp\reportes\prueba.pdf");
-                MessageBox.Show("Exported");
-            }
+            rptDoc = new ReportDocument();
+            rptDoc.Load(filePath);
+            rptDoc.PrintToPrinter(1,false, 0, 0);
         }
     }
 }   
